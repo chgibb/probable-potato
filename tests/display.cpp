15520b410dedbd52323ee7823b843e35f15b7469
Chris Gibb <chris.gibb@outlook.com>
2018-04-09 16:35:11 -0400

    destroy display after test

--- tests/display.cpp
+++ tests/display.cpp
@@ -9,6 +9,7 @@ TEST_CASE( "Should init properly","") {
     REQUIRE(PBP::init() == 0);
     REQUIRE(PBP::initDisplay(640,480) == 0);
     REQUIRE(PBP::display != NULL);
+    PBP::destroyDisplay();
 }
 
 int main(int argc,char*argv[])
